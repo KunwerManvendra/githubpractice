ea2ea6115a4c7f1790cf568fb7cea109c6c52674
Manav Singh <manvendrasingh0128@gmail.com>
2025-01-25 11:48:37 +0530

    added extra print func in lambda func

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+client=boto3.client('ec2')
+
+def lambda_handler(event, context):
+    response=client.run_instances(
+    ImageId='ami-01b799c439fd5516a',
+    InstanceType='t2.micro',
+    MaxCount=1,
+    MinCount=1)
+    #print(response)
+    
+    print(response[Instances][0][InstanceId])
